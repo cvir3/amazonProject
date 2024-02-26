6df87d7a396c14cbd1f0f2827428556fad96a138
cvir3 <vir3.chauhan@gmail.com>
2024-02-26 22:50:32 +0530

    Added List and for loop

--- src/test/java/elements/webEleSearchPage.java
+++ src/test/java/elements/webEleSearchPage.java
@@ -7,4 +7,5 @@ public class webEleSearchPage {
     WebDriver webDriver;
 
     public By searchBoxTxt = By.id("twotabsearchtextbox");
+    public By selectItem = By.xpath("//div[@class='left-pane-results-container']");
 }
--- src/test/java/function/funSearch.java
+++ src/test/java/function/funSearch.java
@@ -1,32 +1,48 @@
 package function;
 
 import elements.webEleSearchPage;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.interactions.SourceType;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.baseClass;
 
 import java.io.IOException;
 import java.time.Duration;
+import java.util.List;
 
 public class funSearch extends baseClass {
     WebDriver webDriver;
     webEleSearchPage ele_s = new webEleSearchPage();
 
     /* This is Constructor */
-    public funSearch(WebDriver remoteDriver){
+    public funSearch(WebDriver remoteDriver) {
         webDriver = remoteDriver;
     }
 
-    public void searchItem() throws  IOException,InterruptedException{
+    public void searchItem() throws IOException, InterruptedException {
         WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
         WebElement searchBox = webDriver.findElement(ele_s.searchBoxTxt);
         wait.until(ExpectedConditions.visibilityOf(searchBox));
         searchBox.sendKeys("Iphone");
-        searchBox.sendKeys(Keys.ENTER);
+        //searchBox.sendKeys(Keys.ENTER);
         Thread.sleep(1000);
+
+        List<WebElement> list = webDriver.findElements(By.xpath("(//div[@class='s-suggestion-container'])[4]"));
+        System.out.println("Find Text::===>" + list.size());
+
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println(list.get(i).getText());
+            if (list.get(i).getText().contains("iphone 15 plus")) {
+                list.get(i).click();
+                //break;
+            }
+        }
     }
 }
+
